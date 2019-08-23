f21bf426dde4fb913d268667677af11ef8d9a396
Sean <writeseanhong@gmail.com>
2019-08-23 17:47:34 -0500

    added checkcollision and blocks

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,6 +1,8 @@
 
 import React, { useState } from "react";
 
+import { createStage, checkCollision } from "../gameHelpers";
+
 //styled components
 import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
 
@@ -16,13 +18,63 @@ const Tetris = () => {
     const [dropTime, setDropTime] = useState(null);
     const [gameOver, setGameOver] = useState(false);
 
-    const [player] = usePlayer();
-    const [stage, setStage] = useStage(player);
+    const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
+    const [stage, setStage] = useStage(player, resetPlayer);
 
     console.log('re-render');
 
+    const movePlayer = dir => {
+        if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+            updatePlayerPos({ x: dir, y: 0 });
+        }
+    }
+
+    const startGame = () => {
+        //Reset everything
+        setStage(createStage());
+        resetPlayer();
+        setGameOver(false);
+    }
+
+    const drop = () => {
+        if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+            updatePlayerPos({ x: 0, y: 1, collided: false })
+        } else {
+            //Game Over
+            if (player.pos.y < 1) {
+                console.log("GAME OVER !!!");
+                setGameOver(true);
+                setDropTime(null);
+            }
+            updatePlayerPos({ x: 0, y: 0, collided: true })
+        }
+
+    }
+
+
+    const dropPlayer = () => {
+        drop();
+
+    }
+    //Destructured event key code passeed in so no need to access event.keyCode inside the move function 
+    const move = ({ event, keyCode }) => {
+        if (!gameOver) {
+            if (keyCode === 37) {
+                movePlayer(-1);
+            } else if (keyCode === 39) {
+                movePlayer(1);
+            } else if (keyCode === 40) {
+                dropPlayer();
+            } else if (keyCode === 83) {
+                playerRotate(stage, 1);
+            } else if (keyCode === 65) {
+                playerRotate(stage, -1);
+            }
+
+        }
+    }
     return (
-        <StyledTetrisWrapper>
+        <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
             <StyledTetris>
                 <Stage stage={stage} />
                 <aside>
@@ -35,7 +87,7 @@ const Tetris = () => {
                                 <Display text="Level" />
                             </div>
                         )}
-                    <StartButton />
+                    <StartButton callback={startGame} />
                 </aside>
             </StyledTetris>
         </StyledTetrisWrapper>
