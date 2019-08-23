cfe6e2e4ed310aa161e9091924c02cf9f29d72d2
Sean <writeseanhong@gmail.com>
2019-08-23 12:08:16 -0500

    finished stage with styled components,button, and scoreboard

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,23 +1,29 @@
 import React from "react";
 
 import { createStage } from "../gameHelpers";
+import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
 
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
+import { StyledCell } from "./styles/StyledCell";
 
-const Tetris = () => (
-    <div>
-        <Stage stage={createStage()} />
-        <aside>
-            <div>
-                <Display text="score" />
-                <Display text="Rows" />
-                <Display text="Level" />
-            </div>
-            <StartButton />
-        </aside>
-    </div>
-)
+const Tetris = () => {
+    return (
+        <StyledTetrisWrapper>
+            <StyledTetris>
+                <Stage stage={createStage()} />
+                <aside>
+                    <div>
+                        <Display text="score" />
+                        <Display text="Rows" />
+                        <Display text="Level" />
+                    </div>
+                    <StartButton />
+                </aside>
+            </StyledTetris>
+        </StyledTetrisWrapper>
+    );
+}
 
 export default Tetris;
