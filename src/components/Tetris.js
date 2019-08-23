97940415038123ffe7b517db62c9d4718f6e6739
Sean <writeseanhong@gmail.com>
2019-08-23 15:07:42 -0500

    set up react hooks for displays and edit readme

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { createStage } from "../gameHelpers";
 import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
+
+import { usePlayer } from "./hooks/usePlayer";
+import { useStage } from "./hooks/useStage";
 
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
-import { StyledCell } from "./styles/StyledCell";
 
 const Tetris = () => {
+    const [dropTime, setDropTime] = useState(null);
+    const [gameOver, setGameOver] = useState(false);
+
+    const [player] = usePlayer();
+    const [stage, setStage] = useStage(player);
+
+    console.log('re-render');
+
     return (
         <StyledTetrisWrapper>
             <StyledTetris>
-                <Stage stage={createStage()} />
+                <Stage stage={stage} />
                 <aside>
-                    <div>
-                        <Display text="score" />
-                        <Display text="Rows" />
-                        <Display text="Level" />
-                    </div>
+                    {gameOver ? (
+                        <Display gameOver={gameOver} text="Game Over" />
+                    ) : (
+                            <div>
+                                <Display text="score" />
+                                <Display text="Rows" />
+                                <Display text="Level" />
+                            </div>
+                        )}
                     <StartButton />
                 </aside>
             </StyledTetris>
