134433935bc37d0fba03463eed24eea05d3fff0a
Sean <writeseanhong@gmail.com>
2019-08-23 18:17:18 -0500

    added set interval to drop the blocks every second

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -7,6 +7,7 @@ import { createStage, checkCollision } from "../gameHelpers";
 import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
 
 //custom hooks
+import { useInterval } from "./hooks/useInterval";
 import { usePlayer } from "./hooks/usePlayer";
 import { useStage } from "./hooks/useStage";
 
@@ -32,6 +33,7 @@ const Tetris = () => {
     const startGame = () => {
         //Reset everything
         setStage(createStage());
+        setDropTime(1000);
         resetPlayer();
         setGameOver(false);
     }
@@ -51,8 +53,16 @@ const Tetris = () => {
 
     }
 
+    const keyUp = ({ keyCode }) => {
+        if (!gameOver) {
+            if (keyCode === 40) {
+                setDropTime(1000);
+            }
+        }
+    }
 
     const dropPlayer = () => {
+        setDropTime(null);
         drop();
 
     }
@@ -73,6 +83,11 @@ const Tetris = () => {
 
         }
     }
+
+    useInterval(() => {
+        drop();
+    }, dropTime)
+
     return (
         <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
             <StyledTetris>
