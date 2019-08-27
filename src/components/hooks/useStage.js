edcfb8d405a380f3732a3df2412b115378db253e
Sean <writeseanhong@gmail.com>
2019-08-26 21:21:05 -0500

    added collision sound effects and line sweep effect

--- src/components/hooks/useStage.js
+++ src/components/hooks/useStage.js
@@ -1,18 +1,30 @@
 import { useState, useEffect } from "react";
 import { createStage } from "../../gameHelpers";
+import collideSound from "../../audio/collideSound.wav";
+import clearRow from "../../audio/clearRow.wav";
 
 export const useStage = (player, resetPlayer) => {
     const [stage, setStage] = useState(createStage());
     const [rowsCleared, setRowsCleared] = useState(0);
 
+
+
     useEffect(() => {
         setRowsCleared(0);
-
+        const audio = new Audio(collideSound);
+        const collidingSound = function () {
+            audio.play();
+        }
+        const audio2 = new Audio(clearRow)
+        const clearRowSound = function () {
+            audio2.play();
+        }
         const sweepRows = newStage =>
             newStage.reduce((acc, row) => {
                 if (row.findIndex(cell => cell[0] === 0) === -1) {
                     setRowsCleared(prev => prev + 1);
                     acc.unshift(new Array(newStage[0].length).fill([0, "clear"]));
+                    clearRowSound();
                     return acc;
                 }
                 acc.push(row);
@@ -38,6 +50,8 @@ export const useStage = (player, resetPlayer) => {
 
             // Check if we collided
             if (player.collided) {
+                // play audio here
+                collidingSound();
                 resetPlayer();
                 return sweepRows(newStage);
             }
